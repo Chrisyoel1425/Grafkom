569a00bf87fa2a7044aed9cb275c99e41b4b7d02
Chrisyoel1425 <c14210046@john.petra.ac.id>
2023-02-13 11:19:35 +0700

    C14210046

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import Engine.Object2d;
+import Engine.ShaderProgram;
 import Engine.Window;
+import org.joml.Vector3f;
 import org.lwjgl.opengl.GL;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.glClearColor;
 import static org.lwjgl.opengl.GL30.*;
@@ -9,14 +17,35 @@ public class Main {
         window.init();
         GL.createCapabilities();
 
-
+        //Code
+        objects.add(new Object2d(
+                        Arrays.asList(
+                                new ShaderProgram.ShaderModuleData(
+                                        "resources/scene.vert", GL_VERTEX_SHADER),
+                                new ShaderProgram.ShaderModuleData(
+                                        "resources/scene.frag", GL_FRAGMENT_SHADER)
+                        ),
+                        new ArrayList<>(
+                                List.of(
+                                        new Vector3f(0.0f, 0.5f, 0.0f),
+                                        new Vector3f(-0.5f, -0.5f, 0.0f),
+                                        new Vector3f(0.5f, -0.5f, 0.0f)
+                                )
+                        )
+                )
+        );
     }
+    private ArrayList<Object2d> objects = new ArrayList<>();
     public void loop(){
         while(window.isOpen()){
             window.update();
             glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
             GL.createCapabilities();
+
             //Code
+            for(Object2d object : objects){
+                object.draw();
+            }
 
             //Restore state
             glDisableVertexAttribArray(0);
